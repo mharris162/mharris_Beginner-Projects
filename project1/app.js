d40581d8b36a04b44d68df23d99d335e6f662b8d
mharris162 <mharris162@student.alamo.edu>
2024-11-04 19:37:33 -0600

    emoji change project completed

--- project1/app.js
+++ project1/app.js
@@ -1,11 +1,19 @@
 const closedFace = document.querySelector('.closed');
-const openFace = document.querySelector('.active');
+const openFace = document.querySelector('.open');
 
 
 //event listener 
 closedFace.addEventListener('click', () => {
     if (openFace.classList.contains('open')) {
         openFace.classList.add('active');
+        closedFace.classList.remove('active');
+    }
+} );
+
+openFace.addEventListener('click', () => {
+    if (closedFace.classList.contains('closed')) {
+        closedFace.classList.add('active');
+        openFace.classList.remove('active');
     }
 } );
 
