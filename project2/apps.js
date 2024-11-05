8afbfd9b202f45f68d7474fd14c05f5f6eb4fb67
mharris162 <mharris162@student.alamo.edu>
2024-11-04 20:00:47 -0600

    up to 15:45 in vid

---
+++ project2/apps.js
@@ -0,0 +1,46 @@
+let data = [
+{
+
+    name: 'Winston Schmidt',
+    age: '42'
+
+},
+{
+
+    name: 'Jessica Day',
+    age: '42'
+
+},
+{
+
+    name: 'Nick Miller',
+    age: '43'
+
+},
+{
+
+    name: 'Cece Parekh',
+    age: '42'
+
+},
+{
+
+    name: 'Winston Bishop',
+    age: '42'
+
+},
+{
+
+    name: 'Reagan Lucas',
+    age: '40'
+
+},
+{
+
+    name: 'Sam Sweeney',
+    age: '45'
+
+},
+];
+
+
