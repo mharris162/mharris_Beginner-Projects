78a3235a5655418d5c8dd4f0d9255c50ab2ba967
mharris162 <mharris162@student.alamo.edu>
2024-11-04 20:19:38 -0600

    up to 24:08

--- project2/apps.js
+++ project2/apps.js
@@ -1,46 +1,35 @@
 let data = [
 {
-
     name: 'Winston Schmidt',
     age: '42'
-
 },
 {
-
     name: 'Jessica Day',
     age: '42'
-
 },
 {
-
     name: 'Nick Miller',
     age: '43'
-
 },
 {
-
     name: 'Cece Parekh',
     age: '42'
-
 },
 {
-
     name: 'Winston Bishop',
     age: '42'
-
 },
 {
-
     name: 'Reagan Lucas',
     age: '40'
-
-},
-{
-
-    name: 'Sam Sweeney',
-    age: '45'
-
-},
+}
 ];
 
 
+const info = document.querySelector('#info');
+
+let details = data.map(function(item) {
+    return '<div>' + item.name + ' is ' + item.age + ' years old' + '</div>';
+});
+
+info.innerHTML = details.join(`\n`);
